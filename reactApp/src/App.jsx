9d89bfd2ac7f43b4eee0e0d85eb449acecefded0
Cyrus Adriel Arellano <77707782+iShyrus@users.noreply.github.com>
2023-07-13 18:05:40 -0700

    Flask-Template

---
+++ reactApp/src/App.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [data, setData] = useState([]);
+
+
+  useEffect(() =>{
+    fetch("http://127.0.0.1:5000/members").then(
+      res => res.json()
+    ).then(
+      data =>{
+        setData(data)
+        console.log(data)
+      }
+    )
+  },[])
+
+  
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default App;
